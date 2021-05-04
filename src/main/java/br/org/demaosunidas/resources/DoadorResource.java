2e141b5a0a4096395ec3cfe15b8dc1c390d7e0b6
Giuliano <giulianodb@gmail.com>
2021-05-04 15:04:34 -0300

    Criação de doador

---
+++ src/main/java/br/org/demaosunidas/resources/DoadorResource.java
@@ -0,0 +1,87 @@
+package br.org.demaosunidas.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.org.demaosunidas.domain.Doador;
+import br.org.demaosunidas.services.DoadorService;
+
+@RestController
+@RequestMapping(value="/doadores")
+public class DoadorResource {
+	
+	@Autowired
+	private DoadorService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	@CrossOrigin
+	public ResponseEntity<Page<Doador>> findPage (
+			@RequestParam(value="page",defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage",defaultValue="200") Integer linesPerPage,
+			@RequestParam(value="orderBy",defaultValue="nome") String orderBy,
+			@RequestParam(value="direction",defaultValue="ASC") String direction,
+			@RequestParam(value="nome",required = false) String nome) {
+		
+		
+		Page<Doador> lista = service.search(nome,page,linesPerPage,orderBy,direction);
+		
+		
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Doador doador){
+		service.insert(doador);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(doador.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id,@RequestBody Doador doador){
+		doador.setId(id);
+		service.update(doador);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(doador.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@CrossOrigin
+	public ResponseEntity<Doador> findById(@PathVariable Integer id){
+			
+		Doador obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@CrossOrigin
+	public ResponseEntity<Doador> Excluir(@PathVariable Integer id){
+			
+		service.deletar(id);
+		
+		return ResponseEntity.noContent().build() ;
+	}
+	
+	
+	
+}
---
+++ src/main/java/br/org/demaosunidas/services/DoadorService.java
@@ -0,0 +1,76 @@
+package br.org.demaosunidas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.org.demaosunidas.domain.Doador;
+import br.org.demaosunidas.domain.Status;
+import br.org.demaosunidas.repository.DoadorRepository;
+import br.org.demaosunidas.services.exception.ObjectNotFoudException;
+
+@Service
+public class DoadorService {
+	
+	@Autowired
+	private DoadorRepository repo;
+	
+	public List<Doador> listar() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+	
+	public Page<Doador> search (String nome, Integer page,Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+				
+		
+		return repo. searchQuery(nome,pageRequest);
+	}
+	
+	public Doador findById(Integer id) {
+		Optional<Doador> obj = repo.findById(id);
+
+		return obj. orElseThrow(() -> new ObjectNotFoudException("Objet non trouvé"));
+	}
+	
+	public void insert(Doador obj) {
+		obj.setId(null);
+		repo.save(obj);
+	}
+	
+	public Doador update(Doador objAlterado) {
+		Doador objBanco = findById(objAlterado.getId());
+		updateData(objBanco,objAlterado);
+		return repo.save(objBanco);
+	}
+	
+	public void deletar (Integer id) {
+		Doador obj = findById(id);
+		obj.setStatus(Status.INATIVO);
+		repo.save(obj);
+	}
+	
+	private void updateData(Doador objBanco, Doador objAnterado) {
+		objBanco.setBairro(objAnterado.getBairro());
+		objBanco.setCelular(objAnterado.getCelular());
+		objBanco.setCidade(objAnterado.getCidade());
+		objBanco.setCpf(objAnterado.getCpf());
+		objBanco.setDataNascimento(objAnterado.getDataNascimento());
+		objBanco.setEmail(objAnterado.getEmail());
+		objBanco.setEstado(objAnterado.getEstado());
+		objBanco.setNome(objAnterado.getNome());
+		objBanco.setRg(objAnterado.getRg());
+		objBanco.setRua(objAnterado.getRua());
+		objBanco.setTelefone(objAnterado.getTelefone());
+		objBanco.setStatus(objAnterado.getStatus());
+		
+	}
+
+	
+	
+}
